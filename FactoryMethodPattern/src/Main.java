a7ac103fc711f67a666fb51480456ad5b611b9e9
Naveen Gowda P R <navpr7@gmail.com>
2025-06-25 17:59:14 +0530

    Delete FactoryMethodPattern directory

--- FactoryMethodPattern/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Shape shape1 = ShapeFactory.getShape("circle");
-        Shape shape2 = ShapeFactory.getShape("square");
-
-        shape1.draw();  // Output: Drawing a Circle
-        shape2.draw();  // Output: Drawing a Square
-    }
-}
--- FactoryMethodPattern/src/Square.java
+++
@@ -1,5 +0,0 @@
-public class Square implements Shape {
-    public void draw() {
-        System.out.println("Drawing a Square");
-    }
-}
